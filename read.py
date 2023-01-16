be640a0c6aced7dee0fcb3651d0c74ea6961c5da
EmmaCwiklak <103240822+EmmaCwiklak@users.noreply.github.com>
2023-01-16 20:19:00 +0100

    testing_0

--- read.py
+++ read.py
@@ -4,10 +4,14 @@ from functions import rescaleFrame
 from UserOutputs import pathInput, selectingEdiction
 
 def main():
-#path=pathInput()
     file = argparse.ArgumentParser()
+
     file.add_argument('-f', '--file', action='store', dest='path_from', default='cat.jpg',help='path to your file')
-    results= file.parse_args()
+    results = file.parse_args()
+
+    rescale = argparse.ArgumentParser()
+    rescale.add_argument('-rs','--rescale', dest='accumulate', action='store',help='rescale displayed image')
+    display = rescale.parse_args()
 
     img = cv.imread(results.path_from)
 
@@ -15,32 +19,9 @@ def main():
     cv.imshow(f'{results}', resized_image)
 
 #selectingEdiction()
+
     cv.waitKey(0) #czeka na jakis
 
 if __name__ == '__main__':
     main()
 
-"""import cv2, argparse
-from functions import rescaleFrame
-from UserOutputs import pathInput, selectingEdiction
-
-def main():
-    file = argparse.ArgumentParser(description='input path to photo')
-
-    file.add_argument('-f', '--file', action='store', dest='path_from', default='cat.jpg', help='path to your file')
-    results = file.parse_args()
-
-    img = cv2.imread(results.path_from)
-    cv2.waitKey(0) #zamknie okno jak cos nacisne
-
-
-    #ustalanie wielkosci okna
-    #resized_image = rescaleFrame(img)
-    #cv2.imshow(f'{results.path_from}', resized_image)
-
-
-    #selectingEdiction()
-
-if __name__ == '__main__':
-    main()
-"""
