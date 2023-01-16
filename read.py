97c96b345b2f3138ae0d29e130734177a04d2d2f
EmmaCwiklak <103240822+EmmaCwiklak@users.noreply.github.com>
2023-01-16 19:40:38 +0100

    testing_0

--- read.py
+++ read.py
@@ -3,22 +3,22 @@ import argparse
 from functions import rescaleFrame
 from UserOutputs import pathInput, selectingEdiction
 
+def main():
 #path=pathInput()
-file = argparse.ArgumentParser()
-file.add_argument('-f', '--file', action='store', dest='path_from',
-                        default='cat.jpg',
-                        help='path to your file')
-results= file.parse_args()
+    file = argparse.ArgumentParser()
+    file.add_argument('-f', '--file', action='store', dest='path_from', default='cat.jpg',help='path to your file')
+    results= file.parse_args()
 
-img = cv.imread(results.path_from)
+    img = cv.imread(results.path_from)
 
-resized_image = rescaleFrame(img)
-cv.imshow(f'{results}', resized_image)
+    resized_image = rescaleFrame(img)
+    cv.imshow(f'{results}', resized_image)
 
 #selectingEdiction()
-cv.waitKey(0) #czeka na jakis
+    cv.waitKey(0) #czeka na jakis
 
-
+if __name__ == '__main__':
+    main()
 
 """import cv2, argparse
 from functions import rescaleFrame
