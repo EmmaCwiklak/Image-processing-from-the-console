a400ca7371cf6dbbb51d8b3ee7539b9a4218afef
EmmaCwiklak <103240822+EmmaCwiklak@users.noreply.github.com>
2023-01-17 20:22:18 +0100

    testing_0

--- functions.py
+++ functions.py
@@ -6,3 +6,22 @@ def rescaleFrame(frame, scale=0.75):
     dimensions = (width, height)
 
     return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+
+def resize(img):
+    image = cv2.imread(img, cv2.IMREAD_UNCHANGED)
+
+    print('Original Dimensions : ', img.shape)
+
+    scale_percent = 60  # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    print('Resized Dimensions : ', resized.shape)
+
+    cv2.imshow("Resized image", resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- read.py
+++ read.py
@@ -4,7 +4,30 @@ from functions import rescaleFrame
 from UserOutputs import pathInput, selectingEdiction
 
 def main():
-    file = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", help='path to your picture')
+    parser.add_argument("--operation", help='choose edit operation')
+
+    args = parser.parse_args()
+    print(args.file)
+    print(args.operation)
+
+    if args.operation == "display":
+        # wyswietlanie obrazu
+        img = cv.imread(args.file)
+        cv.imshow(f'{args.file}', img)
+
+
+
+
+
+    cv.waitKey(0)
+
+if __name__ == '__main__':
+    main()
+
+"""
+file = argparse.ArgumentParser()
 
     file.add_argument('-f', '--file', action='store', dest='path_from', default='cat.jpg',help='path to your file')
     results = file.parse_args()
@@ -21,7 +44,4 @@ def main():
 #selectingEdiction()
 
     cv.waitKey(0) #czeka na jakis
-
-if __name__ == '__main__':
-    main()
-
+"""
