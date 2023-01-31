7d290187b667bd75e2440a91e15225ed8477e9ae
EmmaCwiklak <103240822+EmmaCwiklak@users.noreply.github.com>
2023-02-01 00:09:27 +0100

    testing_0

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 import cv2 as cv
-
+import numpy as np
 def rescaleFrame(img, scale):
     width = int(img.shape[1] * scale)
     height = int(img.shape[0] * scale)
-    dimensions = (width, height)
+    dim = (width, height)
 
-    img = cv.resize(img, dimensions, interpolation=cv.INTER_AREA)
+    img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
     return cv.imshow('default', img)
 def resizing(img, scale_percent):
     print('Original Dimensions : ', img.shape)
@@ -34,5 +34,20 @@ def canny(img):
     image = cv.Canny(img, 125, 175)
     cv.imshow('Canny', image)
     cv.imwrite('canny.jpg', image)
+def translate(img, x, y):
+    image = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, image, dimensions)
+    cv.imshow('Translated', image)
+    cv.imwrite('translated.jpg', image)
+def rotate(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
 
-#def filtrowanie_koloru(img):
+    if rotPoint is None:
+        rotPoint=(width//2, height//2)
+
+    rotated = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotated, dimensions)
+    cv.imshow('Rotated', image)
--- read.py
+++ read.py
@@ -1,17 +1,18 @@
 import cv2 as cv
+import numpy as np
 import argparse
-from functions import rescaleFrame, resizing, gray, blur, canny
+from functions import rescaleFrame, resizing, gray, blur, canny, translate, rotate
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", default='cat.jpg', help='path to your picture')
+    parser.add_argument("--file", default='wsb.jpg', help='path to your picture')
     parser.add_argument("--operation", default='default', help='choose edit operation')
-    parser.add_argument("--x", "-x", help='x', default=5, type=int)
-    parser.add_argument("--y", "-y", help='y', default=5, type=int)
-    parser.add_argument("--r", "-r", help='red', default=255, type=int)
-    parser.add_argument("--g", "-g", help='green', default=255, type=int)
-    parser.add_argument("--b", "-b", help='blue', default=255, type=int)
+    parser.add_argument("--x", "-x", help='x parameter', default=0, type=int)
+    parser.add_argument("--y", "-y", help='y parameter', default=0, type=int)
+    parser.add_argument("--r", "-r", help='red', default=255, type=float)
+    parser.add_argument("--g", "-g", help='green', default=255, type=float)
+    parser.add_argument("--b", "-b", help='blue', default=255, type=float)
     args = parser.parse_args()
     print(args.file)
     print(args.operation)
@@ -25,13 +26,8 @@ def main():
 
     img = cv.imread(args.file)
 
-
     if args.operation == "default":
-        # wyswietlanie obrazu
-        if args.x != 5:
-            rescaleFrame(img, x)
-        else:
-            rescaleFrame(img, 0.5)
+        rescaleFrame(img, 0.5)
     elif args.operation == "resize": #dziala
         resizing(img,x)
 
@@ -51,13 +47,38 @@ def main():
     elif args.operation == "canny": #dziala
         canny(img)
 
-    elif args.operation == "split":
-        merged = cv.merge([b,g,r])
-        cv.imshow("Merged Image", merged)
+    elif args.operation == "translate": #dziala
+        if x==0 and y == 0:
+            x=y=100
+        translated=translate(img,x,y)
+        cv.imshow("Translate",translated)
+
+    elif args.operation == "rotate":
+        rotated = rotate(img,x)
+        cv.imshow("rotated", rotated)
+        cv.imwrite('rotated.jpg', rotated)
+
+    elif args.operation == "flip":
+        flip = cv.flip(img, x)
+        cv.imshow("flip", flip)
+        cv.imwrite('fliped.jpg', flip)
+
+    elif args.operation == "hsv":
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        cv.imshow("HSV", hsv)
+        cv.imwrite('hsv.jpg', hsv)
+    elif args.operation == "lab":
+        lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+        cv.imshow("LAB", lab)
+        cv.imwrite('lab.jpg', lab)
+    elif args.operation == "border_color":
+        COLOR= [b,g,r]
+        constant= cv.copyMakeBorder(img,10,10,10,10,cv.BORDER_CONSTANT,value=COLOR)
+        cv.imshow("border",constant)
+        cv.imwrite("border.jpg",constant)
+
 
     cv.waitKey(0)
 
 if __name__ == '__main__':
     main()
-
-
